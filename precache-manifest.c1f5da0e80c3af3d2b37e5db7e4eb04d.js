18168a127217f3f71a58007436a51bcd8852f891
Zaheer Ahmad <ranazee@gmail.com>
2020-04-14 16:06:02 +0400

    Updates

---
+++ precache-manifest.c1f5da0e80c3af3d2b37e5db7e4eb04d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20920064a64d3e69672f373a11bb1dd1",
+    "url": "/Contact-Manager-Reactjs/index.html"
+  },
+  {
+    "revision": "4249117ef5d2de96aefa",
+    "url": "/Contact-Manager-Reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ee0464ef01245aa428b4",
+    "url": "/Contact-Manager-Reactjs/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4249117ef5d2de96aefa",
+    "url": "/Contact-Manager-Reactjs/static/js/2.3718d8fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager-Reactjs/static/js/2.3718d8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0464ef01245aa428b4",
+    "url": "/Contact-Manager-Reactjs/static/js/main.eebb90b9.chunk.js"
+  },
+  {
+    "revision": "deb4766ec35727a54e84",
+    "url": "/Contact-Manager-Reactjs/static/js/runtime-main.c9d58a37.js"
+  }
+]);
